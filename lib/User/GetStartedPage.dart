0d7dc36bd9504e4b525cbe5a6d55e986dfc40958
manikanta-capsitech <manikanta.saaketh@capsitech.com>
2025-10-06 19:16:40 +0530

    Minor Structuring.

---
+++ lib/User/GetStartedPage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GetStartedPage extends StatefulWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  State<GetStartedPage> createState() => _GetStartedPageState();
+}
+
+class _GetStartedPageState extends State<GetStartedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [Text("Learner Space")],
+      ),
+    );
+  }
+}
